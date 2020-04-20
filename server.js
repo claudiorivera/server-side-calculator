5dc10e6ead545f6e728c3453e31551ed2ea103cf
Claudio Rivera <claudiorivera@me.com>
2020-04-19 21:53:01 -0500

    Changed PORT and created Procfile for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Module imports
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const { calculate } = require("./calculate");
 
 // History of operations
