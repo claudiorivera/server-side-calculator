8a355bf3a06e71a47bac064d996b68e7d15a3c21
Claudio Rivera <me@claudiorivera.com>
2020-05-04 11:42:07 -0500

    Changed express static to use path

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const history = [];
 
 // Instantiate middleware
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(webHookHandler);
 
