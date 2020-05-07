9a9acdef11dac79bbdc17963078f20c1a18587ce
Claudio Rivera <me@claudiorivera.com>
2020-05-07 13:05:16 -0500

    MVP

--- calculate.js
+++ calculate.js
@@ -1,6 +1,6 @@
 // Function takes in two values and an operator (ie. "add", "subtract")
 function calculate(firstValue, secondValue, operation) {
-  // Return a value calculated based on the operator
+  // Return a value calculated based on the operator, as a string
   switch (operation) {
     case "+":
       return String(Number(firstValue) + Number(secondValue));
--- server.js
+++ server.js
@@ -2,17 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
-const webhook = require("express-github-webhook");
-const { exec } = require("child_process");
 
 const { calculate } = require("./calculate");
 
 // Environmental variables
 const PORT = process.env.PORT;
-const SECRET_TOKEN = process.env.SECRET_TOKEN;
 
 const app = express();
-const webHookHandler = webhook({ path: "/webhook", secret: SECRET_TOKEN });
 
 // Global variables
 const history = [];
@@ -21,28 +17,14 @@ const history = [];
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-app.use(webHookHandler);
 
-// WebHooks
-webHookHandler.on("*", function (event, repo, data) {
-  console.log("webHookHandler event: ", event);
-  exec("git pull", (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
-});
-
+// Routes
+// GET /history - Get all calculations
 app.get("/history", (req, res) => {
   res.json(history);
 });
 
+// POST /calculate - Create a new calculation
 app.post("/calculate", (req, res) => {
   // Push new object (with computed result) into the history array
   history.push({
@@ -58,6 +40,7 @@ app.post("/calculate", (req, res) => {
   res.json(history);
 });
 
+// DELETE /history - Delete a calculation from history
 app.delete("/history", (req, res) => {
   history.splice(0, history.length);
   res.json(history);
